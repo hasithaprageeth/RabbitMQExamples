b2a132466871680ec98e4fde26625a47d322e4ff
hasithaprageeth <haztha@gmail.com>
2024-04-05 12:03:01 +0100

    Add RabbitMQPublishSubscribe project

---
+++ RabbitMQPublishSubscribe/Receiver/Receiver.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+///  Documentation : https://www.rabbitmq.com/tutorials/tutorial-three-dotnet
+///  Run more than one instance of the receiver to see the runtime behavior in RabbitMQ
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+// Create a exchange of fanout type
+channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+// Temporary queues
+// Let server to chosse the queue name - non-durable, exclusive, autodelete queue with a generated name
+var queueName = channel.QueueDeclare().QueueName;
+channel.QueueBind(queue: queueName,
+                  exchange: "logs",
+                  routingKey: string.Empty); // Roting key is ignored with fanout exchange
+
+Console.WriteLine("Start listening to messages.");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, eventArgs) =>
+{
+    var body = eventArgs.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Received {message}");
+};
+
+channel.BasicConsume(queue: queueName,
+                     autoAck: true,
+                     consumer: consumer);
+
+// With this line, we keep the program (event handler) running in the background
+Console.ReadLine();
---
+++ RabbitMQPublishSubscribe/Sender/Sender.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+// Create a exchange of fanout type
+// Note : no queue is declared
+channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+for (int i = 0; i < 1000; i++)
+{
+    var body = Encoding.UTF8.GetBytes($"This is message {i + 1} from sender");
+
+    channel.BasicPublish(exchange: "logs",
+                         routingKey: string.Empty, // Roting key is ignored with fanout exchange
+                         basicProperties: null,
+                         body: body);
+
+    Console.WriteLine($"Sent a message {i + 1}");
+    await Task.Delay(TimeSpan.FromSeconds(1));
+}
+
+Console.ReadLine();

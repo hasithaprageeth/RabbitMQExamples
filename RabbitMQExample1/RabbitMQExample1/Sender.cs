db6cd7c499022546a46c852135955c28183a48ff
hasithaprageeth <haztha@gmail.com>
2024-04-05 01:56:43 +0100

    Add RabbitMQExample1 project

---
+++ RabbitMQExample1/RabbitMQExample1/Sender.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory
+{
+    Uri = new Uri("amqp://guest:guest@localhost:5672"),
+    ClientProvidedName = "Sender" // Any preferred name
+};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+var exchangeName = "inputExchange";
+var routingKey = "inputqueue";
+var queueName = "inputqueue";
+
+// Exchange routes the messages directly
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+channel.QueueDeclare(queue: queueName,
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+// Bind queue to the exchange
+channel.QueueBind(queueName, exchangeName, routingKey);
+
+for (int i = 0; i < 10; i++)
+{
+    var body = Encoding.UTF8.GetBytes($"This is message {i + 1} from sender");
+
+    channel.BasicPublish(exchange: string.Empty,
+                     routingKey: routingKey,
+                     basicProperties: null,
+                     body: body);
+
+    Console.WriteLine($"Sent a message {i + 1}");
+    await Task.Delay(TimeSpan.FromSeconds(1));
+}
+
+Console.ReadLine();
+
+channel.Close();
+connection.Close();
---
+++ RabbitMQExample1/Receiver/Receiver.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory
+{
+    Uri = new Uri("amqp://guest:guest@localhost:5672"),
+    ClientProvidedName = "Receiver" // Any preferred name
+};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+var exchangeName = "inputExchange";
+var routingKey = "inputqueue";
+var queueName = "inputqueue";
+
+// Exchange routes the messages directly
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+channel.QueueDeclare(queue: queueName,
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+// Bind queue to the exchange
+channel.QueueBind(queueName, exchangeName, routingKey);
+
+// Fair Dispatch
+channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+Console.WriteLine("Start listening to messages.");
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, eventArgs) =>
+{
+    var body = eventArgs.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Received {message}");
+
+    channel.BasicAck(eventArgs.DeliveryTag, multiple: false);
+};
+
+var consumerTag = channel.BasicConsume(queue: "inputqueue",
+                                       autoAck: false,
+                                       consumer: consumer);
+Console.ReadLine();
+
+// Cancel/deletes the consumer
+channel.BasicCancel(consumerTag);
+
+channel.Close();
+connection.Close();

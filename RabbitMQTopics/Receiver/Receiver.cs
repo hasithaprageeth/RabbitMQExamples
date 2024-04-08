71cf3a7c02b338507b3ebd09a1ae7ec0d09933e4
hasithaprageeth <haztha@gmail.com>
2024-04-08 08:04:36 +0100

    Add RabbitMQTopics Project

---
+++ RabbitMQTopics/Receiver/Receiver.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+///  Documentation : https://www.rabbitmq.com/tutorials/tutorial-five-dotnet
+///  Run more than one instance of the receiver to see the runtime behavior in RabbitMQ
+///  First Receiver - dotnet run "#" - To receive all the logs
+///  Second Receiver - dotnet run "kern.*"  - To receive all logs from the facility "kern"
+///  Third Receiver - dotnet run "*.error"  - To receive all logs from the severity "error"
+///  Or use dotnet run "kern.*" "*.error"
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+// Create an exchange of topic type
+channel.ExchangeDeclare(exchange: "topic_logs", type: ExchangeType.Topic);
+
+// Temporary queues
+// Let server to choose the queue name - non-durable, exclusive, autodelete queue with a generated name
+var queueName = channel.QueueDeclare().QueueName;
+
+if (args.Length < 1)
+{
+    Console.Error.WriteLine("Usage: {0} [binding_key...]",
+                            Environment.GetCommandLineArgs()[0]);
+    Console.WriteLine(" Press [enter] to exit.");
+    Console.ReadLine();
+    Environment.ExitCode = 1;
+    return;
+}
+
+foreach (var severity in args)
+{
+    channel.QueueBind(queue: queueName,
+                      exchange: "topic_logs",
+                      routingKey: severity); // Roting key is <facility>.<severity>
+}
+
+Console.WriteLine("Start listening to messages.");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, eventArgs) =>
+{
+    var body = eventArgs.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Received {eventArgs.RoutingKey} : {message}");
+};
+
+channel.BasicConsume(queue: queueName,
+                     autoAck: true,
+                     consumer: consumer);
+
+// With this line, we keep the program (event handler) running in the background
+Console.ReadLine();
---
+++ RabbitMQTopics/Sender/Sender.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+/// Documentation : https://www.rabbitmq.com/tutorials/tutorial-five-dotnet
+/// Sender - dotnet run
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+// Create a exchange of topic type
+// Note : no queue is declared
+channel.ExchangeDeclare(exchange: "topic_logs", type: ExchangeType.Topic);
+
+string[] facility = ["auth", "cron", "kern"];
+string[] severityStatus = ["info", "warning", "error"];
+Random random1 = new();
+Random random2 = new();
+
+for (int i = 0; i < 1000; i++)
+{
+    int randomIndex1 = random1.Next(0, facility.Length);
+    int randomIndex2 = random1.Next(0, severityStatus.Length);
+    var routingKey = $"{facility[randomIndex1]}.{severityStatus[randomIndex2]}";
+    var body = Encoding.UTF8.GetBytes($"This is message {i + 1} from sender");
+
+    channel.BasicPublish(exchange: "topic_logs",
+                         routingKey: routingKey, // Roting key is <facility>.<severity>
+                         basicProperties: null,
+                         body: body);
+
+    Console.WriteLine($"Sent a message {i + 1}");
+    await Task.Delay(TimeSpan.FromSeconds(1));
+}
+
+Console.ReadLine();
